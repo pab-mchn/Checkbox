5696316cc17df68ab8879d110b1d952511885c9e
pablo michelini <pab.michelini@gmail.com>
2022-05-20 16:26:13 +0200

    characters component done

--- src/components/Characters/Characters.js
+++ src/components/Characters/Characters.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import { useContext } from "react";
+import { dataContext } from "../context/DataContext";
 
 const Characters = () => {
-  return (
-    <h1>Characters</h1>
-  )
-}
+  const { dataCharacters } = useContext(dataContext);
 
-export default Characters
+  return dataCharacters.map((characters) => {
+    return (
+      <div className="rotation" key={characters.id}>
+        <div className="internal-rotation">
+          <div className="front-rotation">
+            <img src={characters.image} alt="" />
+          </div>
+          <div className="back-rotation">
+            <h1>{characters.name}</h1>
+            <p>Status: {characters.status}</p>
+            <p>Species: {characters.species}</p>
+            <p>Gender: {characters.gender}</p>
+          </div>
+        </div>
+      </div>
+    );
+  });
+};
+
+export default Characters;
